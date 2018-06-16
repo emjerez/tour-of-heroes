fdeef56890a989e3fe460cef28ded807ab3ab2f9
Ethel Jerez <ethelmj@gmail.com>
2018-06-16 17:29:40 -0400

    added uppercase pipe to hero name, added two way binding to hero name, added and imported formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
