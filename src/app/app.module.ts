745fffb3c04cd8a904817b31123dd578d5b40f7c
Ethel Jerez <ethelmj@gmail.com>
2018-06-17 14:24:00 -0400

    generated dashboard component, added dashboard route, added default route, added detailhero route, updated herodetailcomponent to be routable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {  HttpClientModule } from  '@angular/common/http';
 
 
 @NgModule({
@@ -14,12 +16,14 @@ import { AppRoutingModule } from './/app-routing.module';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
